26d5bdb279cb06a2970e05256a9b023d9c3fd705
Cuotify <cuotify@gmail.com>
2020-11-25 21:39:11 -0300

    first object JS
    Object InvestmentAssets

---
+++ main.js
@@ -0,0 +1,9 @@
+function InvestmentAsset(newName, newAnnualYield, newEntityName) {
+    this.name = newName;
+    this annualYield = newAnnualYield;
+    this.entityName = newEntityName;
+}
+
+let plazoFijoTradicional = new InvestmentAsset("Plazo fijo tradicional", 37, "Banco Supervielle");
+
+
