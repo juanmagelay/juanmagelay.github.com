781e039f92d390bf362f0740bf7dc5fe889b7130
Cuotify <cuotify@gmail.com>
2020-11-25 21:45:57 -0300

    fix-point-in-object-function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function InvestmentAsset(newName, newAnnualYield, newEntityName) {
     this.name = newName;
-    this annualYield = newAnnualYield;
+    this.annualYield = newAnnualYield;
     this.entityName = newEntityName;
 }
 
