8e1c15e5f15b3c5b521eecf1f44f75f36849c43e
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2020-11-30 22:26:52 -0300

    new property for object investmentAsset

--- main.js
+++ main.js
@@ -2,23 +2,25 @@ var inflation12m = 50;
 var blueDollar12m = 200;
 var blueDollarToday = 158;
 
-function InvestmentAsset(newName, newAnnualYield, newEntityName) {
-    this.name = newName;
+function InvestmentAsset(newAssettName, newAnnualYield, newEntityName, newEntityTipe, newInvestmentType, newAssetCurrency) {
+    this.assetName = newAssettName;
     this.annualYield = newAnnualYield;
     this.entityName = newEntityName;
+    this.entityType = newEntityTipe;
+    this.entityType.investmentType = newInvestmentType;
+    this.assetCurrency = newAssetCurrency;
 }
 
-let plazoFijoTradicionalSupervielle = new InvestmentAsset("Plazo fijo tradicional", 37, "Banco Supervielle");
+let plazoFijoTradicionalSupervielle = new InvestmentAsset("Plazo fijo tradicional", 37, "Supervielle", "Banco tradicional", "Plazo fijo", "Pesos");
 
-let plazoFijoUVASupervielle = new InvestmentAsset("Plazo fijo UVA", 1+inflation12m, "Banco Supervielle");
+let plazoFijoUVASupervielle = new InvestmentAsset("Plazo fijo UVA", 1+inflation12m, "Supervielle", "Banco tradicional", "Plazo fijo", "Pesos");
 
 // ( precio dolar en un ano - precio dolar hoy ) / precio dolar hoy
-let compraDolarOficialHoyYVentaAlAno = new InvestmentAsset("Compra de dólar oficial y venta al año", ((blueDollar12m - blueDollarToday) / blueDollarToday) * 100, "Banco Supervielle");
-
+let compraDolarOficialHoyYVentaAlAnoSupervielle = new InvestmentAsset("Rulo en dólares", ((blueDollar12m - blueDollarToday) / blueDollarToday) * 100, "Banco Supervielle", "Banco tradicional", "Plazo fijo", "Dólares");
 
 console.log(plazoFijoTradicionalSupervielle);
 
 console.log(plazoFijoUVASupervielle);
 
-console.log(compraDolarOficialHoyYVentaAlAno);
+console.log(compraDolarOficialHoyYVentaAlAnoSupervielle);
 
