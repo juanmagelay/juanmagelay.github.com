c3d15f937fe7129364cd998d3bd74cc04e043e0b
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2020-12-15 23:20:48 -0300

    tyding up investment assets objects

--- main.js
+++ main.js
@@ -33,12 +33,40 @@ class InvestmentAsset {
 
 
 //Investment Assets new Objects
-let plazoFijoTradicionalSupervielle = new InvestmentAsset("Plazo fijo tradicional", 37, "Supervielle", "Bancos tradicionales", "Plazo fijo", "Pesos");
-let plazoFijoUVASupervielle = new InvestmentAsset("Plazo fijo UVA", 1+inflation12m, "Supervielle", "Bancos tradicionales", "Plazo fijo", "Pesos");
+let plazoFijoTradicionalSupervielle = new InvestmentAsset(
+    "Plazo fijo tradicional", 
+    37, 
+    "Supervielle", 
+    "Bancos tradicionales", 
+    "Plazo fijo", 
+    "Pesos"
+);
+let plazoFijoUVASupervielle = new InvestmentAsset(
+    "Plazo fijo UVA", 
+    1+inflation12m, 
+    "Supervielle", 
+    "Bancos tradicionales", 
+    "Plazo fijo", 
+    "Pesos"
+);
 // ( precio dolar en un ano - precio dolar hoy ) / precio dolar hoy
-let compraDolarBlueHoyYVentaAlAno = new InvestmentAsset("Rulo con dólar blue", ((blueDollar12m - blueDollarToday) / blueDollarToday) * 100, "Alguna", "Casas de cambio", "Rulos con dólares", "Dólares");
+let compraDolarBlueHoyYVentaAlAno = new InvestmentAsset(
+    "Rulo con dólar blue", 
+    ((blueDollar12m - blueDollarToday) / blueDollarToday) * 100, 
+    "Alguna", 
+    "Casas de cambio", 
+    "Rulos con dólares", 
+    "Dólares"
+);
 // ( precio dolar en un ano - precio dolar hoy ) / precio dolar hoy
-let compraDolarOficialHoyYVentaAlAno = new InvestmentAsset("Rulo con dólar oficial", ((blueDollar12m - oficialDollarToday) / oficialDollarToday) * 100, "Supervielle", "Bancos tradicionales", "Rulos con dólares", "Dólares");
+let compraDolarOficialHoyYVentaAlAno = new InvestmentAsset(
+    "Rulo con dólar oficial", 
+    ((blueDollar12m - oficialDollarToday) / oficialDollarToday) * 100, 
+    "Supervielle", 
+    "Bancos tradicionales", 
+    "Rulos con dólares", 
+    "Dólares"
+);
 //TEST
 console.log(plazoFijoTradicionalSupervielle);
 console.log(plazoFijoUVASupervielle);
