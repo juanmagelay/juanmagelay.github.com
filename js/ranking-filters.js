91d4bc84b081e0cdf0885d215f746fd4f7aa4ab1
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2020-12-23 16:16:24 -0300

    first attempt very bad

---
+++ js/ranking-filters.js
@@ -0,0 +1,10 @@
+/*function generateFilter() {
+    document.getElementById("apply-filters").onclick = saveFilter;
+        for (const element of investmentAssets) {
+            document.getElementById(investmentAssets).onclick = filterEvent;
+        }
+}
+
+generateFilter();*/
+
+
