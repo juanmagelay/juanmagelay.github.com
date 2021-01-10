3c3ff64e78a7fa7dc19a5476495f527406db0427
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2021-01-09 23:25:51 -0300

    json moved to data

---
+++ js/data.js
@@ -0,0 +1,11 @@
+//JSON
+const globalVars = '{"inflation12m":52.5, "blueDollar12m":212.4, "blueDollarToday":144, "oficialDollarToday":82.1, "oficialDollar12m":121.1, "dollarTax":1.65}'; //MANUAL
+
+//Dollar values obtained by API
+const dollarValues = function(){
+    $.get("https://www.dolarsi.com/api/api.php?type=valoresprincipales", 
+    function(posts){
+      console.log(posts);
+    })
+};
+dollarValues();
--- js/globalVars.js
+++ js/globalVars.js
@@ -1,20 +1,7 @@
 //Tooltip
 $('#juanma-profile-photo').tooltip();
 
-//Global vars
-//JSON
-const globalVars = '{"inflation12m":52.5, "blueDollar12m":212.4, "blueDollarToday":144, "oficialDollarToday":82.1, "oficialDollar12m":121.1, "dollarTax":1.65}'; //MANUAL
-
-//Dollar values obtained by API
-const dollarValues = function(){
-    $.get("https://www.dolarsi.com/api/api.php?type=valoresprincipales", 
-    function(posts){
-      console.log(posts);
-    })
-};
-dollarValues();
-
-//OBJECTS
+//Global vars from data
 const inflation12m = JSON.parse(globalVars).inflation12m;
 const blueDollar12m = JSON.parse(globalVars).blueDollar12m;
 const blueDollarToday = JSON.parse(globalVars).blueDollarToday;
