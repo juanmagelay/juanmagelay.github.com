e7bc06339a776e509a9eb53b16a9e0caa8fdb6d3
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2021-01-10 00:07:55 -0300

    the dollar values are being automized, wooooowww!

--- js/data.js
+++ js/data.js
@@ -1,18 +1,25 @@
 //JSON
 const globalVars = [{
-    "inflation12m":52.5, 
-    "blueDollar12m":212.4, 
+    "inflation12m":52.5, //MANUAL
+    "blueDollar12m":212.4, //MANUAL
     "blueDollarToday":144, 
     "oficialDollarToday":82.1, 
-    "oficialDollar12m":121.1, 
-    "dollarTax":1.65
-}]; //MANUAL
+    "oficialDollar12m":121.1, //MANUAL
+    "dollarTax":1.65 //MANUAL
+}]; 
 
 //Dollar values obtained by API
 const dollarValues = function(){
     $.get("https://www.dolarsi.com/api/api.php?type=valoresprincipales", 
     function(posts){
-      console.log(posts);
+      const officialDollarAPI = posts[0].casa.venta;
+      const blueDollarAPI = posts[1].casa.venta;
+      console.log(officialDollarAPI);
+      console.log(blueDollarAPI);
     })
 };
+
 dollarValues();
+
+// console.log(officialDollarAPI); Outside of function doesn't work.
+
