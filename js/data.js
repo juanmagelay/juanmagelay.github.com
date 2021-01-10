585d5ca10bc3c7eb0c1d46a091c66b10ae0a6e54
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2021-01-10 20:50:42 -0300

    push the official dollar value into the main array!!! wow, it is incredible!!

--- js/data.js
+++ js/data.js
@@ -1,15 +1,15 @@
 //JSON
 const globalVars = [{
-    "inflation12m":52.5
-  }, { //MANUAL
-    "blueDollar12m":212.4
-  }, { //MANUAL
+    "inflation12m":52.5 //MANUAL
+  }, { 
+    "blueDollar12m":212.4 //MANUAL
+  }, { 
     "blueDollarToday":144
   }, { 
     "oficialDollarToday":82.1
   }, { 
-    "oficialDollar12m":121.1
-  }, { //MANUAL
+    "oficialDollar12m":121.1 //MANUAL
+  }, { 
     "dollarTax":1.65 //MANUAL
   }
 ]; 
@@ -18,8 +18,10 @@ const globalVars = [{
 const dollarValues = function(){
     $.get("https://www.dolarsi.com/api/api.php?type=valoresprincipales", 
     function(posts){
-      const officialDollarAPI = posts[0].casa.venta;
+      const officialDollarAPI = { officialDollarAP: parseFloat(posts[0].casa.venta.replace(',', '.')) };
       const blueDollarAPI = posts[1].casa.venta;
+      globalVars.push(officialDollarAPI);
+      console.log(globalVars);
       console.log(officialDollarAPI);
       console.log(blueDollarAPI);
     })
