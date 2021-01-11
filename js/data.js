be24066e87bae0b2f9b54e604c967ba9d456d748
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2021-01-11 20:28:58 -0300

    I am only one step away to the result. Please JS God, let me use these values

--- js/data.js
+++ js/data.js
@@ -15,16 +15,12 @@ const globalVars = [{
 ]; 
 
 //Dollar values obtained by API
-const dollarValues = function(){
-    $.get("https://www.dolarsi.com/api/api.php?type=valoresprincipales", 
+$.get("https://www.dolarsi.com/api/api.php?type=valoresprincipales", 
     function(posts){
+        console.log("Obtained data from API", posts)
       const officialDollarToday = { officialDollarToday: parseFloat(posts[0].casa.venta.replace(',', '.')) };
       const blueDollarToday = { blueDollarToday: parseFloat(posts[1].casa.venta.replace(',', '.')) };
-      globalVars.push(officialDollarToday, blueDollarToday);
+      globalVars.splice(4,2,officialDollarToday, blueDollarToday)
       console.log(globalVars);
-      console.log(officialDollarToday);
-      console.log(blueDollarToday);
     })
-};
 
-dollarValues();
