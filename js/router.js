06d6e739810fe45dd7fcdbfc1da7aee007ec548b
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2021-01-19 23:03:00 -0300

    other functions in router

--- js/router.js
+++ js/router.js
@@ -5,6 +5,23 @@ const routes = [
   { path: '/expenses',  component: ExpensesComponent, },
 ];
 
+const parseLocation = () => location.hash.slice(1).toLowerCase() || '/';
+const findComponentByPath = (path, routes) => routes.find(r => r.path == path || undefined);
+
+const router = () => {
+  const path = parseLocation(); 
+  const { component = ErrorComponent } = findComponentByPath(path, routes) || {}; 
+  //VER SI FALTA ALGO
+}
+
+$( window ).on( 'load', function()  {
+  router();
+});
+
+$( window ).on( 'hashchange', function( e ) {
+    router();
+} );
+
 
 
 //JSON
