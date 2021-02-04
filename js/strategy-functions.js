1fd8e8b0f103bab89d5c721157c876cd5c8ea6f7
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2021-02-04 16:13:38 -0300

    strategies

--- js/strategy-functions.js
+++ js/strategy-functions.js
@@ -3,25 +3,25 @@ function updateValuesInStrategyPage() {
     //Three elements to invest
     //PercentajeOfInvestment
     //1
-    let suggestedInvestmentPercentajeOfInvestment1 = document.getElementById("strategy-suggested-1-percentage-of-investment").innerHTML = (25+"%");
+    let suggestedInvestmentPercentajeOfInvestment1 = document.getElementById("strategy-suggested-1-percentage-of-investment").innerHTML = (30+"%");
     //2
-    let suggestedInvestmentPercentajeOfInvestment2 = document.getElementById("strategy-suggested-2-percentage-of-investment").innerHTML = (28+"%");
+    let suggestedInvestmentPercentajeOfInvestment2 = document.getElementById("strategy-suggested-2-percentage-of-investment").innerHTML = (60+"%");
     //3
-    let suggestedInvestmentPercentajeOfInvestment3 = document.getElementById("strategy-suggested-3-percentage-of-investment").innerHTML = (47+"%");
+    let suggestedInvestmentPercentajeOfInvestment3 = document.getElementById("strategy-suggested-3-percentage-of-investment").innerHTML = (10+"%");
     //AssetNames
     //1
-    let suggestedInvestmentAssetName1 = document.getElementById("strategy-suggested-1-asset-name").innerHTML = (fondoMercadoPago.assetName + " " + fondoMercadoPago.entityName); //MANUAL
+    let suggestedInvestmentAssetName1 = document.getElementById("strategy-suggested-1-asset-name").innerHTML = (compraDeDolaresOficiales.assetName + " " + compraDeDolaresOficiales.entityName); //MANUAL
     //2
-    let suggestedInvestmentAssetName2 = document.getElementById("strategy-suggested-2-asset-name").innerHTML = (plazoFijoUVASupervielle.assetName + " " + plazoFijoUVASupervielle.entityName); //MANUAL
+    let suggestedInvestmentAssetName2 = document.getElementById("strategy-suggested-2-asset-name").innerHTML = (plazoFijoTradicionalReba.assetName + " " + plazoFijoTradicionalReba.entityName); //MANUAL
     //3
-    let suggestedInvestmentAssetName3 = document.getElementById("strategy-suggested-3-asset-name").innerHTML = (cajaRemuneradaReba.assetName + " " + cajaRemuneradaReba.entityName); //MANUAL
-    //TotalMoney
+    let suggestedInvestmentAssetName3 = document.getElementById("strategy-suggested-3-asset-name").innerHTML = (fondoMercadoPago.assetName + " " + fondoMercadoPago.entityName); //MANUAL
+    //TotalMoney 
     //1
-    let suggestedInvestmentTotalMoney1 = document.getElementById("strategy-suggested-1-total-money").innerHTML = ("$"+thousandsSeparatorsForMoney(Math.floor(100000 * 0.25))); //MANUAL
+    let suggestedInvestmentTotalMoney1 = document.getElementById("strategy-suggested-1-total-money").innerHTML = ("$"+thousandsSeparatorsForMoney(Math.floor(100000 * 0.30))); //MANUAL
     //2
-    let suggestedInvestmentTotalMoney2 = document.getElementById("strategy-suggested-2-total-money").innerHTML = ("$"+thousandsSeparatorsForMoney(Math.floor(100000 * 0.28))); //MANUAL
+    let suggestedInvestmentTotalMoney2 = document.getElementById("strategy-suggested-2-total-money").innerHTML = ("$"+thousandsSeparatorsForMoney(Math.floor(100000 * 0.60))); //MANUAL
     //3
-    let suggestedInvestmentTotalMoney3 = document.getElementById("strategy-suggested-3-total-money").innerHTML = ("$"+thousandsSeparatorsForMoney(Math.floor(100000 * 0.47))); //MANUAL
+    let suggestedInvestmentTotalMoney3 = document.getElementById("strategy-suggested-3-total-money").innerHTML = ("$"+thousandsSeparatorsForMoney(Math.floor(100000 * 0.10))); //MANUAL
     //Total Annual Yield
     //TotalAnnualYield
     let totalAnnualYield = document.getElementById("strategy-suggested-total-annual-yield").innerHTML = ("+"+Math.floor(annualYieldCalculated())+"%");
@@ -37,9 +37,9 @@ function thousandsSeparatorsForMoney(anAmountOfMoney) {
 
 function annualYieldCalculated() {
     return (
-        (0.25 * fondoMercadoPago.annualYield) + //MANUAL
-        (0.28 * plazoFijoUVASupervielle.annualYield) + //MANUAL
-        (0.47 * cajaRemuneradaReba.annualYield) //MANUAL
+        (0.30 * compraDeDolaresOficiales.annualYield) + //MANUAL
+        (0.60 * plazoFijoTradicionalReba.annualYield) + //MANUAL
+        (0.10 * fondoMercadoPago.annualYield) //MANUAL
     ) 
 }
 
@@ -85,45 +85,45 @@ function strategyBasedOnAmountAndDays(e) {
         if (anAmountValue > 2000 && aDaysValue > 90) {
         console.log(anAmountValue);
         
-            //25% Mercado Pago
+            //25% Plazo fijo UVA Supervielle
             var suggestedInvestmentPercentajeOfInvestment1 = document.getElementById("strategy-suggested-1-percentage-of-investment").innerHTML = (25+"%");
-            var suggestedInvestmentAssetName1 = document.getElementById("strategy-suggested-1-asset-name").innerHTML = (fondoMercadoPago.assetName + " " + fondoMercadoPago.entityName); //MANUAL
+            var suggestedInvestmentAssetName1 = document.getElementById("strategy-suggested-1-asset-name").innerHTML = (plazoFijoUVASupervielle.assetName + " " + plazoFijoUVASupervielle.entityName); //MANUAL
             var suggestedInvestmentTotalMoney1 = document.getElementById("strategy-suggested-1-total-money").innerHTML = ("$"+thousandsSeparatorsForMoney(Math.floor(anAmountValue * 0.25))); //MANUAL
 
-            //28% Plazo fijo UVA
+            //28% Compra dolar oficial
             var suggestedInvestmentPercentajeOfInvestment2 = document.getElementById("strategy-suggested-2-percentage-of-investment").innerHTML = (28+"%"); //MANUAL
-            var suggestedInvestmentAssetName2 = document.getElementById("strategy-suggested-2-asset-name").innerHTML = (plazoFijoUVASupervielle.assetName + " " + plazoFijoUVASupervielle.entityName); //MANUAL
+            var suggestedInvestmentAssetName2 = document.getElementById("strategy-suggested-2-asset-name").innerHTML = (compraDeDolaresOficiales.assetName + " " + compraDeDolaresOficiales.entityName); //MANUAL
             var suggestedInvestmentTotalMoney2 = document.getElementById("strategy-suggested-2-total-money").innerHTML = ("$"+thousandsSeparatorsForMoney(Math.floor(anAmountValue * 0.28))); //MANUAL
             
-            //47% Fondo Supervielle
+            //47% Compra usdc
             var suggestedInvestmentPercentajeOfInvestment3 = document.getElementById("strategy-suggested-3-percentage-of-investment").innerHTML = (47+"%");
-            var suggestedInvestmentAssetName3 = document.getElementById("strategy-suggested-3-asset-name").innerHTML = (cajaRemuneradaReba.assetName + " " + cajaRemuneradaReba.entityName); //MANUAL
+            var suggestedInvestmentAssetName3 = document.getElementById("strategy-suggested-3-asset-name").innerHTML = (compraDeUsdc.assetName + " " + compraDeUsdc.entityName); //MANUAL
             var suggestedInvestmentTotalMoney3 = document.getElementById("strategy-suggested-3-total-money").innerHTML = ("$"+thousandsSeparatorsForMoney(Math.floor(anAmountValue * 0.47))); //MANUAL
 
-            annualYieldCalculated2(0.25, 0.28, 0.47, fondoMercadoPago, plazoFijoUVASupervielle, cajaRemuneradaReba);
-            var totalAnnualYield = document.getElementById("strategy-suggested-total-annual-yield").innerHTML = ("+"+Math.floor(annualYieldCalculated2(0.25, 0.28, 0.47, fondoMercadoPago, plazoFijoUVASupervielle, cajaRemuneradaReba))+"%");
-            var totalAnnualMoney = document.getElementById("strategy-suggested-total-annual-money").innerHTML = ("+$"+thousandsSeparatorsForMoney(Math.floor((annualYieldCalculated2(0.25, 0.28, 0.47, fondoMercadoPago, plazoFijoUVASupervielle, cajaRemuneradaReba)*anAmountValue/100))));
+            annualYieldCalculated2(0.25, 0.28, 0.47, plazoFijoUVASupervielle, compraDeDolaresOficiales, compraDeUsdc);
+            var totalAnnualYield = document.getElementById("strategy-suggested-total-annual-yield").innerHTML = ("+"+Math.floor(annualYieldCalculated2(0.25, 0.28, 0.47, plazoFijoUVASupervielle, compraDeDolaresOficiales, compraDeUsdc))+"%");
+            var totalAnnualMoney = document.getElementById("strategy-suggested-total-annual-money").innerHTML = ("+$"+thousandsSeparatorsForMoney(Math.floor((annualYieldCalculated2(0.25, 0.28, 0.47, plazoFijoUVASupervielle, compraDeDolaresOficiales, compraDeUsdc)*anAmountValue/100))));
         
         } else if (anAmountValue > 2000 && aDaysValue <= 90) {
         
-            // 25% Mercado Pago
-            var suggestedInvestmentPercentajeOfInvestment1 = document.getElementById("strategy-suggested-1-percentage-of-investment").innerHTML = (25+"%");
-            var suggestedInvestmentAssetName1 = document.getElementById("strategy-suggested-1-asset-name").innerHTML = (fondoMercadoPago.assetName + " " + fondoMercadoPago.entityName); //MANUAL
-            var suggestedInvestmentTotalMoney1 = document.getElementById("strategy-suggested-1-total-money").innerHTML = ("$"+thousandsSeparatorsForMoney(Math.floor(anAmountValue * 0.25))); //MANUAL
+            // 30% Compra De Dolares Oficiales
+            var suggestedInvestmentPercentajeOfInvestment1 = document.getElementById("strategy-suggested-1-percentage-of-investment").innerHTML = (30+"%");
+            var suggestedInvestmentAssetName1 = document.getElementById("strategy-suggested-1-asset-name").innerHTML = (compraDeDolaresOficiales.assetName + " " + compraDeDolaresOficiales.entityName); //MANUAL
+            var suggestedInvestmentTotalMoney1 = document.getElementById("strategy-suggested-1-total-money").innerHTML = ("$"+thousandsSeparatorsForMoney(Math.floor(anAmountValue * 0.30))); //MANUAL
         
-            // 50% Plazo fijo
-            var suggestedInvestmentPercentajeOfInvestment2 = document.getElementById("strategy-suggested-2-percentage-of-investment").innerHTML = (50+"%"); //MANUAL
-            var suggestedInvestmentAssetName2 = document.getElementById("strategy-suggested-2-asset-name").innerHTML = (plazoFijoTradicionalSupervielle.assetName + " " + plazoFijoTradicionalSupervielle.entityName); //MANUAL
-            var suggestedInvestmentTotalMoney2 = document.getElementById("strategy-suggested-2-total-money").innerHTML = ("$"+thousandsSeparatorsForMoney(Math.floor(anAmountValue * 0.50))); //MANUAL
+            // 60% Plazo Fijo Tradicional Reba
+            var suggestedInvestmentPercentajeOfInvestment2 = document.getElementById("strategy-suggested-2-percentage-of-investment").innerHTML = (60+"%"); //MANUAL
+            var suggestedInvestmentAssetName2 = document.getElementById("strategy-suggested-2-asset-name").innerHTML = (plazoFijoTradicionalReba.assetName + " " + plazoFijoTradicionalReba.entityName); //MANUAL
+            var suggestedInvestmentTotalMoney2 = document.getElementById("strategy-suggested-2-total-money").innerHTML = ("$"+thousandsSeparatorsForMoney(Math.floor(anAmountValue * 0.60))); //MANUAL
 
-            //25% Dolar oficial
-            var suggestedInvestmentPercentajeOfInvestment3 = document.getElementById("strategy-suggested-3-percentage-of-investment").innerHTML = (25+"%");
-            var suggestedInvestmentAssetName3 = document.getElementById("strategy-suggested-3-asset-name").innerHTML = (compraDeDolaresOficiales.assetName + " " + compraDeDolaresOficiales.entityName); //MANUAL
-            var suggestedInvestmentTotalMoney3 = document.getElementById("strategy-suggested-3-total-money").innerHTML = ("$"+thousandsSeparatorsForMoney(Math.floor(anAmountValue * 0.25))); //MANUAL
+            //10% Fondo Mercado Pago
+            var suggestedInvestmentPercentajeOfInvestment3 = document.getElementById("strategy-suggested-3-percentage-of-investment").innerHTML = (10+"%");
+            var suggestedInvestmentAssetName3 = document.getElementById("strategy-suggested-3-asset-name").innerHTML = (fondoMercadoPago.assetName + " " + fondoMercadoPago.entityName); //MANUAL
+            var suggestedInvestmentTotalMoney3 = document.getElementById("strategy-suggested-3-total-money").innerHTML = ("$"+thousandsSeparatorsForMoney(Math.floor(anAmountValue * 0.10))); //MANUAL
 
-            annualYieldCalculated2(0.25, 0.50, 0.25, fondoMercadoPago, plazoFijoTradicionalSupervielle, compraDeDolaresOficiales);
-            var totalAnnualYield = document.getElementById("strategy-suggested-total-annual-yield").innerHTML = ("+"+Math.floor(annualYieldCalculated2(0.25, 0.50, 0.25, fondoMercadoPago, plazoFijoTradicionalSupervielle, compraDeDolaresOficiales))+"%");
-            var totalAnnualMoney = document.getElementById("strategy-suggested-total-annual-money").innerHTML = ("+$"+thousandsSeparatorsForMoney(Math.floor((annualYieldCalculated2(0.25, 0.50, 0.25, fondoMercadoPago, plazoFijoTradicionalSupervielle, compraDeDolaresOficiales)*anAmountValue/100))));
+            annualYieldCalculated2(0.30, 0.60, 0.10, compraDeDolaresOficiales, plazoFijoTradicionalReba, fondoMercadoPago);
+            var totalAnnualYield = document.getElementById("strategy-suggested-total-annual-yield").innerHTML = ("+"+Math.floor(annualYieldCalculated2(0.30, 0.60, 0.10, compraDeDolaresOficiales, plazoFijoTradicionalReba, fondoMercadoPago))+"%");
+            var totalAnnualMoney = document.getElementById("strategy-suggested-total-annual-money").innerHTML = ("+$"+thousandsSeparatorsForMoney(Math.floor((annualYieldCalculated2(0.30, 0.60, 0.10, compraDeDolaresOficiales, plazoFijoTradicionalReba, fondoMercadoPago)*anAmountValue/100))));
         
         } else if (anAmountValue < 1000 && aDaysValue >= 0) {
             
